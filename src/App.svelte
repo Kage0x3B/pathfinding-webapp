071709b317b4f18eb74c7ea7ac44282abe122a45
Moritz Hein <moritz.hein@live.de>
2022-06-20 21:54:50 +0200

    Initial commit, vite+svelte+ts template

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang='ts'>
+    import { Card, Col, Container, Input, Row } from 'sveltestrap';
+    import { onMount } from 'svelte';
+    import { GridMap } from './lib/pathfinding/GridMap';
+
+    let gridMapCanvas: HTMLCanvasElement;
+    let gridMap: GridMap;
+
+    onMount(() => {
+        const context = gridMapCanvas.getContext("2d");
+    });
+</script>
+
+<Container>
+    <Row class='my-4'>
+        <Col xs='12'>
+            <Card body>
+                <Row>
+                    <Col xs='6' lg='2'>
+                        <Input type='select' name='select' id='algorithmSelect'>
+                            <option value='dijkstra' selected>Dijkstra</option>
+                            <option value='a-star'>A-Star</option>
+                            <option value='ida-star'>IDA-Star</option>
+                        </Input>
+                    </Col>
+                </Row>
+            </Card>
+        </Col>
+    </Row>
+    <Row>
+        <Col>
+            <Card body>
+                <canvas bind:this={gridMapCanvas}></canvas>
+            </Card>
+        </Col>
+    </Row>
+</Container>
+
---
+++ src/lib/pathfinding/GridMap.ts
@@ -0,0 +1,23 @@
+export class GridMap {
+    private readonly width: number;
+    private readonly height: number;
+    private readonly tiles: number[][];
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.tiles = [];
+
+        for (let x = 0; x < 0; x++) {
+
+        }
+    }
+
+    draw() {
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+
+            }
+        }
+    }
+}
